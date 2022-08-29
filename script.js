accb32013c4098f53ba227bd60ac28d2e52c2869
WESLLEY SANTOS SILVA <weslley.ssilva5@senacsp.edu.br>
2022-08-29 09:45:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function backBlue(){
+    let element = document.querySelector(".style-text")
+    element.style.backgroundColor = "blue"
+}
+
+function backTrans(){
+    let element = document.querySelector(".style-text")
+    element.style.backgroundColor = "transparent"
+}
+
+function fonteVer(){
+    let element = document.querySelector(".style-text")/*sempre usar (") e (.) para selecionar uma classe*/
+    element.style.color = "red"
+}
+function fonteNorm(){
+    let element = document.querySelector(".style-text")
+    element.style.color = "black"
+}
